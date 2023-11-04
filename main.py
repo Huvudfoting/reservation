03ce4d7e13a3f45a8e7465664c22a8b516dd51c3
daniel.routovaara <daniel.routovaara@gmail.com>
2023-11-04 13:59:14 +0100

    Build names and draw randomly

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+adj_file = "data/adjectives.txt"
+noun_file = "data/nouns.txt"
+used_file = "data/used.txt"
 
 
-# Press the green button in the gutter to run the script.
+def read_file(path: str) -> list[str]:
+    f = open(path)
+    return [line.strip() for line in f.readlines()]
+
+
+def build_names() -> list[str]:
+    adjectives = read_file(adj_file)
+    nouns = read_file(noun_file)
+    names = {f'{a} {n}' for a in adjectives for n in nouns}
+    used = set(read_file(used_file))
+    free = names - used
+    return list(free)
+
+
+def draw(seq: list[str], amount: int) -> list[str]:
+    amount = min(amount, len(seq))
+    return random.sample(seq, amount)
+
+
+def print_names():
+    available = build_names()
+    print(len(available))
+    print(available)  # Press Ctrl+F8 to toggle the breakpoint.
+    sample = draw(available, 5)
+    print(sample)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print_names()
